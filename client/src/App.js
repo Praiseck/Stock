f496da3242bf7d489290b0d68cd432ebcb7358fc
Manuel Duque Valencia <manferduval15@gmail.com>
2023-11-20 14:10:50 -0500

    Panel Productos v0

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,98 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  const [Nombre_producto, setNombre_producto] = useState("");
+  const [Descripcion, setDescripcion] = useState("");
+  const [Precio_unidad, setPrecio_unidad] = useState("");
+  const [Cantidad_Stock, setCantidad_Stock] = useState("");
+  const [ProductList, setProductList] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get").then((response) => {
+      setProductList(response.data);
+    });
+  }, []);
+
+  const submitProduct = () => {
+    Axios.post("http://localhost:3001/api/insert", {
+      Nombre: Nombre_producto,
+      Descripcion: Descripcion,
+      PrecioUnitario: Precio_unidad,
+      CantidadEnStock: Cantidad_Stock,
+    });
+
+    setProductList([
+      ...ProductList,
+      {
+        Nombre: Nombre_producto,
+        Descripcion: Descripcion,
+        PrecioUnitario: Precio_unidad,
+        CantidadEnStock: Cantidad_Stock,
+      },
+    ]);
+  };
+
   return (
     <div className="App">
       <h1>PRODUCTOS</h1>
-      <div className='form'>
+      <div className="form">
         <label>Nombre</label>
-        <input type="text" name="Nombre_producto" />
+        <input
+          type="text"
+          name="Nombre_producto"
+          onChange={(n) => setNombre_producto(n.target.value)}
+        />
         <label>Descripcion</label>
-        <input type="text" name="Descripcion" />
+        <input
+          type="text"
+          name="Descripcion"
+          onChange={(d) => setDescripcion(d.target.value)}
+        />
         <label>Precio Unidad</label>
-        <input type="text" name="Precio_unidad" />
+        <input
+          type="text"
+          name="Precio_unidad"
+          onChange={(p) => setPrecio_unidad(p.target.value)}
+        />
         <label>Cantidad en Stock</label>
-        <input type="text" name="Cantidad_Stock" />
+        <input
+          type="text"
+          name="Cantidad_Stock"
+          onChange={(s) => setCantidad_Stock(s.target.value)}
+        />
 
-        <button>Ingresar</button>
+        <button onClick={submitProduct}>Ingresar</button>
       </div>
+      <table>
+          <thead>
+            <tr>
+              <th>Código</th>
+              <th>Nombre</th>
+              <th>Descripcion</th>
+              <th>Cantidad en Inventario</th>
+              <th>Precio unitario</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {ProductList.map((val) => {
+              return (
+                <tr key={val.id}>
+                  <td>{val.ProductoID}</td>
+                  <td>{val.Nombre}</td>
+                  <td>{val.Descripcion}</td>
+                  <td>{val.CantidadEnStock}</td>
+                  <td>{val.PrecioUnitario}</td>
+                  <td>
+                    <button>Editar</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const mysql = require('mysql');
 
@@ -9,17 +11,30 @@ const db = mysql.createPool({
     database: 'pos_system'
 })
 
-app.get('/', (req, res) => {
-    const sqlInsert = "INSERT INTO Producto (Nombre, Descripcion, PrecioUnitario, CantidadEnStock) VALUES ('Producto 1', 'Descripción del producto 1', 100.00, 10);"
-    db.query(sqlInsert, (err, result) => {
-        if (err) {
-            console.error("Error al insertar en la base de datos:", err);
-            res.status(500).send("Error al insertar en la base de datos");
-        } else {
-            console.log("Inserción exitosa:", result);
-            res.send('Hello Monos');
-        }
-    });
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.get('/api/get', (req, res) => {
+
+    const sqlSelect = "SELECT * FROM Producto"
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    })
+});
+
+app.post('/api/insert', (req, res) => {
+
+    const Nombre = req.body.Nombre;
+    const Descripcion = req.body.Descripcion;
+    const PrecioUnitario = req.body.PrecioUnitario;
+    const CantidadEnStock = req.body.CantidadEnStock;
+
+    const sqlInsert = "INSERT INTO Producto (Nombre, Descripcion, PrecioUnitario, CantidadEnStock) VALUES (?,?,?,?);"
+    db.query(sqlInsert, [Nombre, Descripcion, PrecioUnitario, CantidadEnStock], (err, result) => {
+
+    })
 });
 
 app.listen(3001, () => {
